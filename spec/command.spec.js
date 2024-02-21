b74bcfd9ddde8340a030314bd3a7ca3887d39ef5
chrysanthemum <asingiser@gmail.com>
2024-02-21 11:57:04 -0500

    begin testing

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,9 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  it("constructor sets command type", function() {
+    let testObj = new Command('coolType')
+    expect(testObj.commandType).toBe('coolType')
+  });
 
 });
