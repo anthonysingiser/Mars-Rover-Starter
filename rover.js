b3c8f5bdd8e090ebc937a1cf053197ce3e1b98ce
chrysanthemum <asingiser@gmail.com>
2024-02-24 11:57:19 -0500

    add tests

--- rover.js
+++ rover.js
@@ -1,7 +1,14 @@
 class Rover {
    constructor(position){
-      this.position = position
+      this.position = position;
+      this.mode = 'NORMAL';
+      this.generatorWatts = 110;
+   }
+   receiveMessage(message){
+      
+
    }
 }
+
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,7 +8,30 @@ const Command = require('../command.js');
 
 describe("Rover class", function () {
   const testRover = new Rover(1432);
-  it("has a key 'position' with a number value", function () {
-    expect(typeof testRover.position).toBe('number')
+  let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+  let message = new Message('Test message with two commands', commands);
+  let response = testRover.receiveMessage(message);
+
+  it('constructor sets position and default values for mode and generatorWatts', function () {
+    expect(testRover.position).toEqual(1432);
+    expect(testRover.mode).toEqual('NORMAL');
+    expect(testRover.generatorWatts).toEqual(110);
   })
+
+  it('response returned by receiveMessage contains name of the message', function(){
+    expect(response.message).toEqual('Test message with two commands'); 
+  })
+
+  it('response returned by receiveMessage includes two results if two commands are sent in the message', function(){
+    expect(response.results.length).toEqual(2);  
+  })
+
+  it('responds corresctly to the status check command', function(){
+    expect(response.results[1].roverStatus.mode).toEqual('LOW_POWER');
+  })
+
+  it('responds correctly to the mode change command', function(){
+    
+  })
+
 });
